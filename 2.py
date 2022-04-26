b730a030e19084ff67b4151a1b3ee8c6670d3e8e
Omar <o.moreta@gmail.com>
2022-04-25 20:08:39 -0400

    sort and sorted solutions

--- 2.py
+++ 2.py
@@ -1,1 +1,17 @@
 # Write your solution for algorithm 2 below
+unsorted_numerical_list = [6, 1, 9, 2, 5, 3, 8, 7, 4]
+
+unsorted_numerical_list.sort()
+print(unsorted_numerical_list)
+
+unsorted_numerical_list_two = [6, 1, 9, 2, 5, 3, 8, 7, 4]
+
+def sorting_method():
+    print(sorted(unsorted_numerical_list_two))
+
+sorting_method()
+
+unsorted_numerical_list_three = [6, 1, 9, 2, 5, 3, 8, 7, 4]
+
+new_list = sorted(unsorted_numerical_list_three, reverse=True)
+print(new_list)
